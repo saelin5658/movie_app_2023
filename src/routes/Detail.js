930adc2b8937dfb0c831ac31263adebfa1f9cc43
saelin5658 <saelin5658@gmail.com>
2023-03-09 10:27:07 +0900

    home

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'//로케이션객체 저장하는 속성
+import { useLocation, useNavigate } from 'react-router-dom'//로케이션객체 저장하는 속성 객체처럼 사용
 import '../styles/Detail.css'
 
 
 function Detail() {
   const location = useLocation();
   console.log(location);
+
+  //주소창이 
+  const navigate = useNavigate();//
+  if(location.state === undefined){//state에 값이 없을 경우(주소창에 임의로 글자만 넣어서 주소를 찾을수 없을 때(전달받은 state가 없다))
+    navigate('/');//홈으로 이동 즉 리다이렉트 기능이다(홈컴포넌트로 이동)
+  }
+
   const {genres,id,poster,summary,title,year} = location.state;
   return (
     <div className='detail'>
--- src/routes/Home (before).js
+++ src/routes/Home (before).js
@@ -1,34 +1,24 @@
 import axios from 'axios';
-import React, { Component, useEffect, useState } from 'react'
+import React, { Component } from 'react'
 import Movie from '../components/Movie';
 import'../styles/home.css';
 
 
 //최상위 컴포먼트를 가져와서 사용한다.
-function Home(){
-  // state = {
-    // isLoading : true,
-    // movies:[],
-  // }
-  const[isLoading,setIsLoading] = useState(true);//setstate에 해당되는 함수를 만들어서 사용
-  //속성 속성을변경할때(setstate)사용할이름 = 초기값
-  const[movies,setMovies] = useState([]);
+export class Home extends Component {
 
-  // componentDidMount(){
-  //   //주소를 불러오는 시점(영화 api)
-  //   // setTimeout(()=>{
-  //   //   this.setState({isLoading:false});//6초 뒤에 이즈로딩을 false로 바꿈
-  //   // },6000)
-  //   this.getMovies();//함수호출
-  // }
-
-  useEffect(()=>{
-    getMovies();
-  },[]);//주소가 불러와지는 시점
-  //[]값이 바뀔때마다 실행하는것이 업데이트값(둘다 지원)
-
-
-  const getMovies = async () => {//async 비동기식
+  state = {
+    isLoading : true,
+    movies:[],
+  }
+  componentDidMount(){
+    //주소를 불러오는 시점(영화 api)
+    // setTimeout(()=>{
+    //   this.setState({isLoading:false});//6초 뒤에 이즈로딩을 false로 바꿈
+    // },6000)
+    this.getMovies();//함수호출
+  }
+  getMovies = async () => {//async 비동기식
     const {//구조분해할당으로 배열의 안에 movies만 가져오게
       data : {
         data : {
@@ -39,17 +29,15 @@ function Home(){
     } =
     await axios.get('https://yts-proxy.now.sh/list_movies.json?genre=animation&sort_by=like_count');
     //json파일 뒤에 ?를 붙이면 옵션을 붙일수 있음 genre=animation&sort_by=like 장르=옵션,조건(정렬)=좋아요순  사이트에서 정해져 있음
-    // console.log(movies);
-    // this.setState({
-    //   isLoading : false,
-    //   movies,//movies : movies, 속성과 속성값이 같으면 하나만 써도
-    // })
-    setIsLoading(false);//this.setState
-    setMovies(movies);
+    console.log(movies);
+    this.setState({
+      isLoading : false,
+      movies,//movies : movies, 속성과 속성값이 같으면 하나만 써도
+    })
   }
 
-
-    // const{isLoading,movies} = this.state;//구조분해할당
+  render() {
+    const{isLoading,movies} = this.state;//구조분해할당
     return (
       <section className='container'>
         {isLoading ? //삼항연산자 사용
@@ -72,7 +60,7 @@ function Home(){
         }
       </section>
     )
-  
+  }
 }
 
 export default Home
